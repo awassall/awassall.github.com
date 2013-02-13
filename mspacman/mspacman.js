2ee7d6dab82f70bcd4b8e0c1f2d3485bac2832d7
Aaron Wassall <aaron.wassall@tufts.edu>
2013-02-12 20:04:15 -0500

    testing onload functions

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,17 +1,22 @@
+function draw(ctx,img,a,b,c,d,e,f)
+{
+	ctx.drawImage(img, a, b, c, d, e, f, c, d);
+}
+
 function gameBoard()
 {
 	var c = document.getElementById("board");
 	if (c.getContext) {
 		var context = c.getContext("2d");
 		var board = new Image();
-		board.src = 'pacman10-hp-sprite.png';
-		context.drawImage(board, 322, 2, 464, 136, 0, 0, 464, 136);
 		var mspacman = new Image();
-		mspacman.src = 'pacman10-hp-sprite.png';
-		context.drawImage(mspacman, 84, 3, 13, 14, 175, 29, 13, 14);
 		var ghost1 = new Image();
+		board.onload = draw(context, board, 322, 2, 464, 136, 0, 0, 464, 136);
+		mspacman.onload = draw(context, mspacman, 322, 2, 464, 136, 0, 0, 464, 136);
+		ghost1.onload = draw(context, ghost1, 322, 2, 464, 136, 0, 0, 464, 136);
+		board.src = 'pacman10-hp-sprite.png';
+		mspacman.src = 'pacman10-hp-sprite.png';
 		ghost1.src = 'pacman10-hp-sprite.png';	
-		context.drawImage(ghost1, 3, 123, 14, 14, 181, 70, 14, 14);
 	}
 	else {
 		alert("Sorry, canvas is not supported on your browser!");
