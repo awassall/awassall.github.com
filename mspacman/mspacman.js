85e1c5d36ab3932e0c62152674f1269de30cfa72
Aaron Wassall <aaron.wassall@tufts.edu>
2013-02-12 23:46:43 -0500

    changed lab 6 code

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -3,17 +3,13 @@ function gameBoard()
 	var c = document.getElementById("board");
 	if (c.getContext) {
 		var context = c.getContext("2d");
-		var board = new Image();
-		var mspacman = new Image();
-		var ghost1 = new Image();
-		board.src = 'pacman10-hp-sprite.png';
-		mspacman.src = 'pacman10-hp-sprite.png';
-		ghost1.onload = function() {
-			context.drawImage(board, 322, 2, 464, 136, 0, 0, 464, 136);
-			context.drawImage(mspacman, 84, 3, 13, 14, 175, 29, 13, 14);
-			context.drawImage(ghost1, 3, 123, 14, 14, 181, 70, 14, 14);
+		var sprite = new Image();
+		sprite.onload = function() {
+			context.drawImage(sprite, 322, 2, 464, 136, 0, 0, 464, 136);
+			context.drawImage(sprite, 84, 3, 13, 14, 175, 29, 13, 14);
+			context.drawImage(sprite, 3, 123, 14, 14, 181, 70, 14, 14);
 		}
-		ghost1.src = 'pacman10-hp-sprite.png';	
+		sprite.src = 'pacman10-hp-sprite.png';	
 	}
 	else {
 		alert("Sorry, canvas is not supported on your browser!");
