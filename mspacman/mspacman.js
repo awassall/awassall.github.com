840a78b8e8b1d25dc0261d2595878711e3ce6161
Aaron Wassall <aaron.wassall@tufts.edu>
2013-02-12 20:05:12 -0500

    testing onload functions

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -12,10 +12,10 @@ function gameBoard()
 		var mspacman = new Image();
 		var ghost1 = new Image();
 		board.onload = draw(context, board, 322, 2, 464, 136, 0, 0, 464, 136);
-		mspacman.onload = draw(context, mspacman, 322, 2, 464, 136, 0, 0, 464, 136);
-		ghost1.onload = draw(context, ghost1, 322, 2, 464, 136, 0, 0, 464, 136);
 		board.src = 'pacman10-hp-sprite.png';
+		mspacman.onload = draw(context, mspacman, 322, 2, 464, 136, 0, 0, 464, 136);
 		mspacman.src = 'pacman10-hp-sprite.png';
+		ghost1.onload = draw(context, ghost1, 322, 2, 464, 136, 0, 0, 464, 136);
 		ghost1.src = 'pacman10-hp-sprite.png';	
 	}
 	else {
