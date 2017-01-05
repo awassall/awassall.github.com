be5e2513fd26170740f8a719416b3216966e4126
Aaron Wassall <aaron_wassall@yahoo.com>
2017-01-05 18:07:26 -0500

    Test

--- RedLine/javascript/mycode.js
+++ RedLine/javascript/mycode.js
@@ -33,11 +33,12 @@ function fetchStopListByRouteId(route_id) {
         console.log(obj.direction);
         */
         $.each(data, function(k1,v1) {
-            console.log(k1);
             if (k1 == "direction") {
+                console.log(k1);
                 console.log(v1);
                 $.each(v1, function(k2,v2) {
                     console.log(k2);
+                    console.log(v2);
                     if (k2 == "direction_name") {
                         /*
                         if (v2 == "Northbound") {
