ce38056e852e12a92742647dbaa814e172f76e9f
Aaron Wassall <aaron_wassall@yahoo.com>
2017-02-02 18:59:47 -0500

    select list

--- RedLine/javascript/mycode.js
+++ RedLine/javascript/mycode.js
@@ -6,17 +6,6 @@ function failWithError(error_text) {
     document.getElementById("loadStatus").innerHTML = error_text;
 }
 
-function decideWhatToDo(element_id) {
-    var ele = document.getElementById(element_id);
-    if ((ele.innerHTML).search("<span>") >= 0) {
-        //compress back to just the stop name
-        ele.innerHTML = (ele.innerHTML).split("<span>")[1].split("</span>")[0];
-    } else {
-        //populate with stop data
-        fetchStopData(element_id);
-    }
-}
-
 function drawStopTimesToPage(stop_array) {
     var i = 0;
     var dircount = 0;
@@ -28,8 +17,6 @@ function drawStopTimesToPage(stop_array) {
     var time = "";
     var stops = "";
     var div = document.getElementById("stopDataDiv");
-    var numdirs = 0;
-    var dirspan = "";
     //clear div contents
     div.innerHTML = "";
     var dirTab = "";
@@ -45,19 +32,13 @@ function drawStopTimesToPage(stop_array) {
             dircount = 0; //reset counter
             if (i != 0) {
                 //if this isn't the first datapoint, then we need to post the existing table to the page
-                //dirspan = document.createElement("div");
-                //dirspan.setAttribute("class","dirspan" + (numdirs%2));
-                numdirs = numdirs + 1;
-                //dirspan.appendChild(dirTabHead);
-                //dirspan.appendChild(dirTab);
-                //div.appendChild(dirspan);
                 div.appendChild(dirTabHead);
                 div.appendChild(dirTab);
             }
             dirTab = document.createElement("table");
             dirTab.setAttribute("class","dataTableRoot");
             dirTabHead = document.createElement("div");
-            dirTabHead.setAttribute("class","dataTableHeader dirspan" + (numdirs%2));
+            dirTabHead.setAttribute("class","dataTableHeader");
             if (dir == 0) {
                 dirTabHead.innerHTML = "Southbound";
             } else if (dir == 1) {
@@ -94,83 +75,14 @@ function drawStopTimesToPage(stop_array) {
         dirTabTimeCell.innerHTML = time;
     }
     //append the last direction
-    //dirspan = document.createElement("div");
-    //dirspan.setAttribute("class","dirspan" + (numdirs%2));
-    //dirspan.appendChild(dirTabHead);
-    //dirspan.appendChild(dirTab);
-    //div.appendChild(dirspan);
     div.appendChild(dirTabHead);
     div.appendChild(dirTab);
     console.log(stop_array);
 }
 
-// OLD TABLE IMPLEMENTATION
-/*
-function drawStopTimesToPage(element_id, stop_array) {
-    var i = 0;
-    var dircount = 0;
-    var dir = 0;
-    var olddir = 0;
-    var temparray = new Array();
-    var mins = 0;
-    var secs = 0;
-    var time = "";
-    var stops = "";
-    var ele = document.getElementById(element_id);
-    var span = '<span>' + ele.innerHTML + '</span>';
-    ele.innerHTML = span;
-    var dirTab = "";
-    var dirTabNameCell = "";
-    var dirTabTimeCell = "";
-    for (i = 0; i < (stop_array.length); i++) { //only show max of 3 in each direction
-        temparray = stop_array[i];
-        dir = stop_array[i][0];
-        if (i == 0) {
-            olddir = dir; //initialize olddir to the first direction we come across
-            //initialize the first table
-            //dirTab = document.createElement("table");
-            //MORE WORK NEEDS TO GO HERE
-            //  TABLE START
-            //  HEADER IN ONE COLOR
-            //  ROWS IN ANOTHER COLOR
-            //  TABLE END
-            //  REPEAT FOR ADDITIONAL DIRECTIONS
-        } else {
-            if (dir != olddir) {
-                olddir = dir;
-                dircount = 0; //reset counter
-            }
-        }
-        if (dircount < 3) {
-            dircount = dircount + 1;
-        } else {
-            continue;
-        }
-        mins = Math.floor((stop_array[i][1])/60);
-        secs = (stop_array[i][1])%60;
-        if ((mins == 0) && (secs == 0)) { //arriving now
-            time = "Arriving";
-        } else { //there is some nonzero amount of time remaining until arrival
-            if (mins == 0) { //time remaining is only seconds
-                time = secs + ' sec';
-            } else if (secs == 0) { //time remaining is only minutes
-                time = mins + ' min';
-            } else { //time remaining is in both minutes and seconds
-                time = mins + ' min' + ' ' + secs + ' sec';
-            }
-        }
-        stops = stops + '<br/>' + time + ' ' + stop_array[i][2];
-    }
-    ele.innerHTML = ele.innerHTML + stops;
-    console.log(stop_array);
-}
-*/
-
 function fetchStopData(stop_logical_name) {
-    //console.log("fetchStopData: " + stop_logical_name);
     var url = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=" + apiKey + "&stop=" + stop_logical_name + "&format=json";
     var jqxhr = $.getJSON(url).done(function(data) {
-        //alert(data);
         var i = 0;
         var j = 0;
         var k = 0;
@@ -186,42 +98,23 @@ function fetchStopData(stop_logical_name) {
         $.each(data, function(k1,v1) {
             if (k1 == "mode") {
                 for (i = 0; i < (v1.length); i++) {
-                    //console.log("Mode loop: " + i);
                     $.each(v1[i], function(k2,v2) {
-                        //console.log(k2);
-                        //console.log(v2);
                         if (k2 == "mode_name") {
                             if (v2 == "Subway") {
                                 foundSubway = 1;
-                                //console.log("Found Subway");
                                 return true;
                             } else {
-                                // not a subway, skip to the next one
-                                //console.log("Not a subway");
                                 return false;
                             }
                         }
-                        /*
-                        if ((k2 == "mode_name") && (v2 == "Subway")) {
-                            // this might be the red line, but it could be any subway line
-                            // we still have to check if it is the red line
-                            foundSubway = 1;
-                            //breakModeLoop = 1;
-                            return true; //continue
-                        }
-                        */
                         if ((k2 == "route") && (foundSubway == 1)) {
                             foundSubway = 0; //reset to 0
                             // we found a subway, but is it the red line?
                             // if it is not, then we will have to keep searching
-                            //console.log(v2);
                             for (j = 0; j < (v2.length); j++) {
-                                //console.log("Route #" + j);
-                                //console.log(v2[j]);
                                 $.each(v2[j], function(k3,v3) {
                                     if (k3 == "route_id") {
                                         if (v3 == "Red") {
-                                            //console.log("found the red line");
                                             breakRouteLoop = 1;
                                             breakModeLoop = 1;
                                             foundRedLine = 1;
@@ -231,11 +124,9 @@ function fetchStopData(stop_logical_name) {
                                     }
                                     if ((k3 == "direction") && (foundRedLine == 1)) {
                                         for (k = 0; k < 2; k++) {
-                                            //console.log(k);
                                             $.each(v3[k], function(k4,v4) {
                                                 if (k4 == "trip") {
                                                     for (l = 0; l < (v4.length); l++) {
-                                                        //console.log(v4[l]);
                                                         tripHeadsign = "";
                                                         preAway = 0;
                                                         tripArray = new Array();
@@ -256,19 +147,6 @@ function fetchStopData(stop_logical_name) {
                                             });
                                         }
                                     }
-                                    /*
-                                    if ((k3 == "route_id") && (v3 == "Red")) {
-                                        foundRoute = 1;
-                                        breakRouteLoop = 1;
-                                        return true; //continue
-                                    }
-                                    */
-                                    /*
-                                    if ((k3 == "direction") && (foundRoute == 1)) {
-                                        console.log(v3);
-                                        return false;
-                                    }
-                                    */
                                 });
                                 if (breakRouteLoop == 1) {
                                     break;
@@ -284,86 +162,32 @@ function fetchStopData(stop_logical_name) {
             }
         });
         //sort the results
-        //console.log(results);
         var sortedResults = new Array();
         //loop over existing results
         for (i = 0; i < (results.length); i++) {
-            //console.log("i="+i);
-            //console.log(results[i]);
-            //console.log(sortedResults);
-            /*
-            k = i[0];
-            preAway = i[1];
-            tripHeadsign = i[2];
-            */
             if (sortedResults.length == 0) {
                 sortedResults.push(results[i]);
                 continue;
             }
             //loop over new results array to see where to put it
             //sortedResults.length is greater than 0
-            //console.log(sortedResults);
             for (j = 0; j < (sortedResults.length); j++) {
-                //console.log("j="+j);
-                //console.log(results[i][0]);
-                //console.log(results[i][1]);
-                //console.log(sortedResults[j][0]);
-                //console.log(sortedResults[j][1]);
                 if ((results[i][0] <= sortedResults[j][0]) && (results[i][1] <= sortedResults[j][1])) {
                     //insert here
-                    //console.log("here1");
                     sortedResults.splice(j,0,results[i]);
                     break;
                 }
                 if ((j+1) == (sortedResults.length)) {
-                    //console.log("here2");
                     sortedResults.push(results[i]);
                     break;
                 }
             }
         }
-        //drawStopTimesToPage(stop_logical_name,sortedResults);
         drawStopTimesToPage(sortedResults);
     }).fail(function() {
         alert("ERROR: $.getJSON() failed for fetchStopData().");
     });
 }
-
-// THIS IS THE INITIAL TABLE IMPLEMENTATION
-/*
-function drawDataToPage(res) {
-    var i = 0;
-    var dt = document.getElementById("datatable");
-    var temp_stop_array = new Array();
-    var lastindex = 1;
-    for (i = 0; i < (res.length); i++) {
-        var stop_name = (res[i]).split("*")[0]; //this is the display name of the stop
-        if (stop_name.length <= 0) { //name should not be NULL
-            continue; //should never get here in practice...
-        }
-        if ($.inArray(stop_name,temp_stop_array) >= 0) { //skip duplicates
-            continue;
-        } else { //add to temp array for checking against on subsequent iterations
-            temp_stop_array.push(stop_name);
-            lastindex += 1;
-        }
-        var row = dt.insertRow(-1);
-        row.setAttribute("class","row" + (lastindex%2));
-        var cell_stop_name = row.insertCell(0);
-        cell_stop_name.setAttribute("class","cell0");
-        var parent_station = (res[i]).split("*")[1]; //this is the logical name of the stop
-        if (parent_station.length <= 0) { //parent station should not be NULL
-            cell_stop_name.setAttribute("id","noParentStation");
-            cell_stop_name.onclick = function() { alert("ERROR: Null parent station."); };
-        } else {
-            cell_stop_name.setAttribute("id",parent_station);
-            cell_stop_name.onclick = function() { decideWhatToDo(this.id); };
-        }
-        cell_stop_name.innerHTML = stop_name;
-    }
-    document.getElementById("loadStatus").innerHTML = "1";
-}
-*/
 
 // THIS IS THE SELECT LIST IMPLEMENTATION
 function drawDataToPage(res) {
@@ -390,7 +214,6 @@ function drawDataToPage(res) {
             option.disabled = 1;
         } else {
             option.setAttribute("id",parent_station);
-            //cell_stop_name.onclick = function() { decideWhatToDo(this.id); };
         }
         option.text = stop_name;
         select.add(option);
