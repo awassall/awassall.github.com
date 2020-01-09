22242f4a67b121bcfe4b50fd0a33ff4ac5f1cf44
Aaron Wassall <awassall@users.noreply.github.com>
2020-01-09 14:33:55 -0500

    Update main.js

--- wizardsunite/js/main.js
+++ wizardsunite/js/main.js
@@ -1,3 +1,8 @@
+function CFImages_init() {
+  selectedItem = ""; /* this is a global variable */
+  return;
+}
+
 function CFImagesButtonClick(element) {
   var name = element.innerHTML;
   var id = 'table-' + name.replace(/ /g,'-');
