44f67e17647f94d7520f9f10e3f14845976457ca
Aaron Wassall <awassall@users.noreply.github.com>
2020-01-09 14:43:12 -0500

    Update main.js

--- wizardsunite/js/main.js
+++ wizardsunite/js/main.js
@@ -6,9 +6,9 @@ function CFImages_init() {
 function CFImagesButtonClick(element) {
   var name = element.innerHTML;
   var id = 'table-' + name.replace(/ /g,'-');
-  document.getElementById(id).setAttribute("class","CFImagesTable showMe");
+  document.getElementById(id).setAttribute("class","CFImagesTable");
   if (selectedItem != "") {
-    document.getElementById(selectedItem).setAttribute("class","CFImagesTable");
+    document.getElementById(selectedItem).setAttribute("class","CFImagesTable hidden");
   }
   selectedItem = id; /* update the global variable */
   return;
