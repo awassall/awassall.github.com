40a00f1f3a87f371dee14b5412f7a7b1dbfc2767
Aaron Wassall <awassall@users.noreply.github.com>
2020-01-09 14:24:22 -0500

    Update main.js

--- wizardsunite/js/main.js
+++ wizardsunite/js/main.js
@@ -1,4 +1,7 @@
 function CFImagesButtonClick(element) {
-  alert(element.innerHTML);
+  var name = element.innerHTML;
+  var id = 'table-' + name.replace(/ /g,'-');
+  var tableElement = document.getElementById(id);
+  tableElement.setAttribute("class","test");
   return;
 }
