b155a64465e28ce1e736328a21cdc36aa7b7e9cc
Aaron Wassall <awassall@users.noreply.github.com>
2020-04-19 08:32:44 -0400

    Update main.js

--- AnimalCrossing/js/main.js
+++ AnimalCrossing/js/main.js
@@ -1,107 +1,114 @@
 function doSearchBugs() {
   /* initialization */
-  var EntireList = document.getElementById("EntireBugList");  // the <div> with all the bugs
-  var ListLength = EntireList.children.length;  // total number of bugs
-  var bug = "";
+  var EntireList = "";  // the <div> with all the bugs
+  var ListLength = "";  // total number of bugs
+  var critter = "";
   var name = "";
   var price = "";
   var location = "";
   var hours = "", hourStart = "", hourEnd = "", hourSuffix = "";
   var months = "", monthStart = "", monthEnd = "", monthLast = "", monthCurrent = "";
   var header = "", row = "", cell = "", cellText = "";
-  var field = "";
-  var hemisphere = "";
+  var field = "", c = "";
+  var searchCritter = "", searchHemisphere = "";
   
   /* see what we're searching for */
   console.log("Search criteria:");
   console.log(getRadioValue("CritterCategory"));
+  searchCritter = getRadioValue("CritterCategory");
   console.log(getRadioValue("Availability"));
   console.log(getRadioValue("Hemisphere"));
-  hemisphere = getRadioValue("Hemisphere"); // "NorthHemisphere" or "SouthHemisphere"
+  searchHemisphere = getRadioValue("Hemisphere"); // "NorthHemisphere" or "SouthHemisphere"
   
   /* prepare search output table */
   var SearchOutputTable = document.getElementById("SearchOutputTable");
   SearchOutputTable.innerHTML = ""; // empty the table of any previous search
   
   /* run the query */
-  for (var i=0; i<ListLength; i++) {
-    bug = (EntireList.children[i].innerHTML).split("*");
-    name = bug[0];
-    price = bug[1];
-    location = bug[2];
-    hours = bug[3];
-    if (hemisphere == "NorthHemisphere") { months = bug[4]; } // northern hemisphere
-    else { months = bug[5]; } // southern hemisphere
-    row = SearchOutputTable.insertRow();  // append new row at the bottom
-    // NAME
-    cell = row.insertCell();
-    cell.innerHTML = name;
-    // PRICE
-    cell = row.insertCell();
-    cell.innerHTML = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    // LOCATION
-    cell = row.insertCell();
-    cell.innerHTML = location;
-    // HOURS
-    cell = row.insertCell();
-    hours = hours.split(",");
-    if (hours.length == 24) { // whole day
-      cell.innerHTML = "All day";
-    } else {
-      hourStart = parseInt(hours[0]);
-      if (hourStart<12) { hourSuffix = "am"; }
-      else { hourSuffix = "pm"; }
-      hourStart = (((hourStart + 11) % 12) + 1);
-      cellText = hourStart + hourSuffix;
-      hourEnd = parseInt(hours[hours.length-1]);
-      if (hourEnd<12) { hourSuffix = "am"; }
-      else { hourSuffix = "pm"; }
-      hourEnd = (((hourEnd + 11) % 12) + 1);
-      cellText += " - " + hourEnd + hourSuffix;
-      cell.innerHTML = cellText;  // store it
-    }
-    // MONTHS
-    cell = row.insertCell();
-    months = months.split(",");
-    if (months.length == 12) {  // all year
-      cell.innerHTML = "All year";
-    } else {
-      // this is more complicated than hours, because there can be multiple (disconnected) date ranges
-      // but we know they're always in order, so that helps
-      cellText = "";
-      monthStart = monthLast = parseInt(months[0]);
-      for (var m=1; m<months.length; m++) {
-        monthCurrent = parseInt(months[m]);
-        if (m == (months.length-1)) { //this is the last month in the whole set
-          if ((monthCurrent == (monthLast+1)) || ((monthLast-monthCurrent) == 11)) {  // this ends an ongoing range
-            cellText += intToMonth(monthStart) + "-" + intToMonth(monthCurrent) + ", ";
-          } else {  // it's a standalone month
-            cellText += intToMonth(monthCurrent) + ", ";
-          }
-        } else {  // there are still more entries ahead of this one
-          if ((monthCurrent == (monthLast+1)) || ((monthLast-monthCurrent) == 11)) {  // still part of the same range
-            monthLast = monthCurrent; // reset monthLast
-            continue;
-          }
-          else {
-            // we hit the end of this range, time to log it
-            if (monthStart == monthLast) {  // it was just 1 month
-              cellText += intToMonth(monthStart) + ", ";
-            } else {  // it was a range of months
-              cellText += intToMonth(monthStart) + "-" + intToMonth(monthLast) + ", ";
-            }
-            // set the stage for the next range
-            monthStart = monthLast = monthCurrent;
-          }
-        } // end ELSE
-      } // end FOR
-      if (cellText == "") { // there was only 1 month
-        cellText = intToMonth(monthStart) + ", ";
+  for (c of ["Bugs","Fish"]) {
+    if (searchCritter.indexOf(c) == -1) { continue; } // skip if the search criteria exclude this critter
+    if (c == "Bugs") { EntireList = document.getElementById("EntireBugList"); } // bugs
+    else { EntireList = document.getElementById("EntireFishList"); }  // fish
+    ListLength = EntireList.children.length;
+    for (var i=0; i<ListLength; i++) {
+      critter = (EntireList.children[i].innerHTML).split("*");
+      name = critter[0];
+      price = critter[1];
+      location = critter[2];
+      hours = critter[3];
+      if (searchHemisphere == "NorthHemisphere") { months = critter[4]; } // northern hemisphere
+      else { months = critter[5]; } // southern hemisphere
+      row = SearchOutputTable.insertRow();  // append new row at the bottom
+      // NAME
+      cell = row.insertCell();
+      cell.innerHTML = name;
+      // PRICE
+      cell = row.insertCell();
+      cell.innerHTML = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      // LOCATION
+      cell = row.insertCell();
+      cell.innerHTML = location;
+      // HOURS
+      cell = row.insertCell();
+      hours = hours.split(",");
+      if (hours.length == 24) { // whole day
+        cell.innerHTML = "All day";
+      } else {
+        hourStart = parseInt(hours[0]);
+        if (hourStart<12) { hourSuffix = "am"; }
+        else { hourSuffix = "pm"; }
+        hourStart = (((hourStart + 11) % 12) + 1);
+        cellText = hourStart + hourSuffix;
+        hourEnd = parseInt(hours[hours.length-1]);
+        if (hourEnd<12) { hourSuffix = "am"; }
+        else { hourSuffix = "pm"; }
+        hourEnd = (((hourEnd + 11) % 12) + 1);
+        cellText += " - " + hourEnd + hourSuffix;
+        cell.innerHTML = cellText;  // store it
       }
-      cellText = cellText.substr(0,cellText.length-2);  // remove trailing ", "
-      cell.innerHTML = cellText;
-    } // end ELSE
-  } // end of FOR loop
+      // MONTHS
+      cell = row.insertCell();
+      months = months.split(",");
+      if (months.length == 12) {  // all year
+        cell.innerHTML = "All year";
+      } else {
+        // this is more complicated than hours, because there can be multiple (disconnected) date ranges
+        // but we know they're always in order, so that helps
+        cellText = "";
+        monthStart = monthLast = parseInt(months[0]);
+        for (var m=1; m<months.length; m++) {
+          monthCurrent = parseInt(months[m]);
+          if (m == (months.length-1)) { //this is the last month in the whole set
+            if ((monthCurrent == (monthLast+1)) || ((monthLast-monthCurrent) == 11)) {  // this ends an ongoing range
+              cellText += intToMonth(monthStart) + "-" + intToMonth(monthCurrent) + ", ";
+            } else {  // it's a standalone month
+              cellText += intToMonth(monthCurrent) + ", ";
+            }
+          } else {  // there are still more entries ahead of this one
+            if ((monthCurrent == (monthLast+1)) || ((monthLast-monthCurrent) == 11)) {  // still part of the same range
+              monthLast = monthCurrent; // reset monthLast
+              continue;
+            }
+            else {
+              // we hit the end of this range, time to log it
+              if (monthStart == monthLast) {  // it was just 1 month
+                cellText += intToMonth(monthStart) + ", ";
+              } else {  // it was a range of months
+                cellText += intToMonth(monthStart) + "-" + intToMonth(monthLast) + ", ";
+              }
+              // set the stage for the next range
+              monthStart = monthLast = monthCurrent;
+            }
+          } // end ELSE
+        } // end FOR
+        if (cellText == "") { // there was only 1 month
+          cellText = intToMonth(monthStart) + ", ";
+        }
+        cellText = cellText.substr(0,cellText.length-2);  // remove trailing ", "
+        cell.innerHTML = cellText;
+      } // end ELSE
+    } // end of inner FOR loop
+  } // end of outer FOR loop
   
   /* add table headers */
   header = SearchOutputTable.createTHead()
