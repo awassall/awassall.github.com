7de58def55ba6291b545aebc5513b2ed8f11211c
Aaron Wassall <awassall@users.noreply.github.com>
2023-01-15 21:43:42 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ function toggleMenu()
  var anchor = document.getElementById("topbar-menu-anchor");
  var menu = document.getElementById("topbar-menu-img");
  // Change menu icon coloring
- if (menu.className == "menu-hide") {
+ if (anchor.className == "menu-hide") {
   // Show menu
   menu.style.filter = "invert(0)";
  } else {
