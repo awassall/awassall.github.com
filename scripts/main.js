126a291ac12025cb54c495781baefa417a83861b
Aaron Wassall <awassall@users.noreply.github.com>
2023-01-15 22:07:04 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,12 @@
 function toggleMenu()
 {
  var anchor = document.getElementById("topbar-menu-anchor");
- var img = document.getElementById("topbar-menu-img");
  // Change menu icon coloring
  if (anchor.className == "menu-hide") {
   // Show menu
-  img.style.filter = "none";
   anchor.className = "menu-show";
-  alert("menu now shown");
  } else {
   // Hide menu
-  img.style.filter = "invert(1)";
   anchor.className = "menu-hide";
-  alert("menu now hidden");
  }
 }
